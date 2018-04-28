f2bb93389c59281b70016044a32b15982ab80551
itsimply <lukasz.fedor.projekty@gmail.com>
2018-04-28 22:59:55 +0200

    Custom logo support

--- functions.php
+++ functions.php
@@ -65,6 +65,9 @@ function stanleywp_setup() {
 		'default-image' => '',
 	) ) );
 
+	// add custom logo support
+	add_theme_support( 'custom-logo' );
+
 	// Add theme support for selective refresh for widgets.
 	add_theme_support( 'customize-selective-refresh-widgets' );
 }
